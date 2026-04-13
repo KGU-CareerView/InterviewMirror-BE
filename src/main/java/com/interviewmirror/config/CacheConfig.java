d7c82c24f1f988710831b4809c225df6afd62710
taeho89 <xogh080907@gmail.com>
2026-04-13 15:12:25 +0900

    config: add Redis cache configuration

---
+++ src/main/java/com/interviewmirror/config/CacheConfig.java
@@ -0,0 +1,24 @@
+package com.interviewmirror.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(30))
+                .disableCachingNullValues();
+
+        return RedisCacheManager.create(connectionFactory);
+    }
+}
